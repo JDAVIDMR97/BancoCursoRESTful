a5153ee44d267663e8b5f4b02ff7927a95967ecb
a892973 <jesus-david.mendozaramos.external@atos.net>
2023-03-30 12:39:02 -0600

    Add project files.

---
+++ Application/Feautres/Clientes/Commands/UpdateClienteCommand/UpdateClienteCommandValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Feautres.Clientes.Commands.UpdateClienteCommand
+{
+    public class UpdateClienteCommandValidator : AbstractValidator <UpdateClienteCommand>
+    {
+        public UpdateClienteCommandValidator()
+        {
+            RuleFor(r => r.Id).NotEmpty().WithMessage("{PropertyName} No debe estar vacio");
+
+            RuleFor(p => p.Nombre)
+                    .NotEmpty().WithMessage("{PropertyName} no puede ser vacio")
+                    .MaximumLength(80).WithMessage("{PropertyName} no debe exceder de {MaxLength} caracters");
+
+            RuleFor(p => p.Apellido)
+                    .NotEmpty().WithMessage("{PropertyName} no puede ser vacio")
+                    .MaximumLength(80).WithMessage("{PropertyName}no debe exceder de {MaxLength} caracters");
+
+            RuleFor(p => p.FechaNacimiento)
+                    .NotEmpty().WithMessage("Fecha de Nacimiento no puede ser vacia");
+
+            RuleFor(p => p.Telefono)
+                    .NotEmpty().WithMessage("{PropertyName} no puede ser vacio")
+                    .Matches(@"^\d{4}-\d{4}$").WithMessage("{PropertyName} debe cumplir el formato 0000-0000")
+                    .MaximumLength(9).WithMessage("{PropertyName} no debe exceder de {MaxLength} caracters");
+
+            RuleFor(p => p.Email)
+                    .NotEmpty().WithMessage("{PropertyName} no puede ser vacio")
+                    .EmailAddress().WithMessage("{PropertyName} debe ser una direccion de email valida")
+                    .MaximumLength(100).WithMessage("{PropertyName}no debe exceder de {MaxLength} caracters");
+
+            RuleFor(p => p.Direccion)
+                  .NotEmpty().WithMessage("{PropertyName} no puede ser vacio")
+                  .MaximumLength(120).WithMessage("{PropertyName}no debe exceder de {MaxLength} caracters");
+        }
+    }
+}
---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Application.Behaviours;
+using Application.Feautres.Clientes.Commands.CreateClienteCommand;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyContainer
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateClienteCommand>());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        }
+    }
+}
